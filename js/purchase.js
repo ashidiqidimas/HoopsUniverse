337b2044a86c6781e0e6c2f32da5be0064e79804
ashidiqidimas <prabowo6@gmail.com>
2021-06-13 15:37:42 +0700

    initial commit

---
+++ js/purchase.js
@@ -0,0 +1,13 @@
+let searchBar = $("#dropdown-trigger");
+
+// searchBar.addEventListener("focus", () => {
+//     alert("test");
+//     });
+
+// searchBar.add('focus', function (event) {
+//    alert("yes");
+// });
+
+function tes() {
+    alert("test");
+};
