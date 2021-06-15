b45d8489ecf4a7a787aea03529dc4b56e74f5e8e
ashidiqidimas <prabowo6@gmail.com>
2021-06-15 11:42:15 +0700

    added size section in heading

--- js/purchase.js
+++ js/purchase.js
@@ -1,5 +1,5 @@
-let searchBar = $("#dropdown-trigger");
-
+// let searchBar = $("#dropdown-trigger");
+//
 // searchBar.addEventListener("focus", () => {
 //     alert("test");
 //     });
@@ -8,6 +8,46 @@ let searchBar = $("#dropdown-trigger");
 //    alert("yes");
 // });
 
-function tes() {
-    alert("test");
-};
+function test(btn) {
+    alert(`#${btn}`);
+}
+
+// Size Selection
+function updateSizeLabels(btn) {
+    // alert("clicked " + btnId);
+    let sizeButtons = $(".size-option button");
+    sizeButtons.removeClass("selected-type");
+    // let target = `#${btn}`;
+    // console.log(target);
+    let selectedType = $(`#${btn}`);
+    selectedType.addClass("selected-type");
+}
+
+function selectedSize(btn) {
+    let sizes = $(".size button");
+    sizes.removeClass("selected-size");
+    let selectedSize = $(`#${btn}`);
+    selectedSize.addClass("selected-size")
+}
+
+// Quantity selection
+function increment(btn) {
+    let counterLabel = $("#current-quantity");
+    let counterInString = counterLabel.html();
+    var counterInInt = parseInt(counterInString);
+    counterInInt++;
+    document.getElementById("current-quantity").innerText = counterInInt.toString();
+}
+
+function decrement(btn) {
+    let counterLabel = $("#current-quantity");
+    let counterInString = counterLabel.html();
+    var counterInInt = parseInt(counterInString);
+    counterInInt--;
+
+    if (counterInInt === 0) {
+        alert("Sorry you have to choose at least 1 item to order");
+    } else {
+        document.getElementById("current-quantity").innerText = counterInInt.toString();
+    }
+}
